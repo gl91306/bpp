58e137cb021cba9b2abf1c200d228a834d31bcec
HackerDaGreat57 <contact.akshatsingh@gmail.com>
2022-07-17 14:06:52 -0700

    sync

--- src/bpp.c
+++ src/bpp.c
@@ -1,18 +1,10 @@
 //bpp.c: The starting point of Blender++.
-#define GLFW_INCLUDE_VULKAN
+#include <glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
 int main(int arc, char** argv) {
-  if (glfwVulkanSupported())
-  {
-    printf("Vulkan is available.\n");
-  }
-
-  if (!glfwVulkanSupported())
-  {
-    printf("Vulkan is not available. Please try running the application on a newer computer.\n");
-  }
+  
 
   printf("\nBlender++ returned 0.\n");
   return 0;
