071382a2d8b742c27b2379eca08bfa406f1861fa
HackerDaGreat57 <contact.akshatsingh@gmail.com>
2022-07-19 12:45:42 -0700

    Plenty of progress made

--- src/include/bpp/bpp.hpp
+++ src/include/bpp/bpp.hpp
@@ -4,21 +4,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#define VER_STRING	"0.0.0.9"
-#define VER_MAJOR	0
-#define VER_MINOR	0
-#define VER_RELEASE	0
-#define VER_BUILD	9
-#define COMPANY_NAME	"HackerDaGreat57 Software"
-#define FILE_VERSION	"0.0.0.9"
-#define FILE_DESCRIPTION	"NuklearToolz - Windows x64"
-#define INTERNAL_NAME	"nktlz.exe"
-#define LEGAL_COPYRIGHT	"(c) HackerDaGreat57 Software 2022. All rights reserved."
-#define LEGAL_TRADEMARKS	"(Not trademarked)"
-#define ORIGINAL_FILENAME	"nktlz.exe"
-#define PRODUCT_NAME	"NuklearToolz"
-#define PRODUCT_VERSION	"0.0.0.9"
-
 std::string glVersion = std::string((char *)glGetString(GL_VERSION));
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void process_input(GLFWwindow *window);
