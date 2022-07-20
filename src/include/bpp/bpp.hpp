bce67549aca51e96d0ca2a7b1916e08a23844b8e
HackerDaGreat57 <contact.akshatsingh@gmail.com>
2022-07-20 12:02:55 -0700

    a

--- src/include/bpp/bpp.hpp
+++ src/include/bpp/bpp.hpp
@@ -1,12 +1,17 @@
 //bpp.hpp: The list of things that Blender++ uses
 #define GLFW_DLL
+
 #include "../glad.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 namespace bpp {
+  namespace variables {
+    short int active_renderer;
+  }
+
   namespace functions {
-    void check_params();
+    void check_params(int pargc, char **pargv);
     inline void framebuffer_size_callback(GLFWwindow *window, int width, int height);
     inline void process_input(GLFWwindow *window);
   }
