b72d2e12b42232aa31800528f7992e5a086abb89
Akshat Singh <HackerDaGreat57@gmail.com>
2022-09-05 21:29:45 -0700

    Almost halfway done with build system

--- scripts/build.windows.bat
+++ scripts/build.windows.bat
@@ -11,17 +11,27 @@ if exist obj (
 ) else (
   mkdir obj
 )
+if exist zlib1.dll (
+  echo DLLs already exist inside bin directory
+) else (
+  mkdir obj
+)
+xcopy "%BPP_COMPILER%/mingw64/bin/*.dll" "%BPP_ROOT%/build" /f /g /y
 
 ::Number of cores is divided by 2 for keeping the system responsive/preventing Intel Hyperthreading to confuse us
 set /A a = %NUMBER_OF_PROCESSORS%
 set /A b = a/2
 echo Building with %b% cores.
 
-for /f "tokens=* delims=" %%x in (rsrc/buildnumber.txt) do (
- set %x%+=1
-)
+set /p content=<rsrc/buildnumber.txt
+set /A content=content+1
+(echo %content%)>rsrc/buildnumber2.txt
 
-echo %x%
+cd rsrc
+del buildnumber.txt
+rename buildnumber2.txt buildnumber.txt
+
+cd %BPP_ROOT%
 
 ::x86 - Generic
-%BPP_COMPILER%/mingw64/bin/mingw32-make -f makefile.windows -j%b% -B FLAGS="-w -Wall -Wextra -pedantic -fpermissive -pg -g3 -Ofast -flto -m64 -mavx2 -static-libstdc++ -static-libgcc"
+%BPP_COMPILER%/mingw64/bin/mingw32-make -f makefile.windows -j%b% FLAGS="-w -Wall -Wextra -pedantic -fpermissive -pg -g3 -Ofast -flto -m64 -mavx2"
