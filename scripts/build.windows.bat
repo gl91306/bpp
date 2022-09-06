fb214b3b81436417437068f423b90764a4b81ce5
Akshat Singh <HackerDaGreat57@gmail.com>
2022-09-05 21:13:53 -0700

    Switch to the other laptop

--- scripts/build.windows.bat
+++ scripts/build.windows.bat
@@ -1,13 +1,27 @@
 ::Build Blender++ for Windows
 setlocal enableextensions enabledelayedexpansion
 cd %BPP_ROOT%
-mkdir bin
-mkdir obj
+if exist bin (
+  echo bin directory already exists
+) else (
+  mkdir bin
+)
+if exist obj (
+  echo obj directory already exists
+) else (
+  mkdir obj
+)
 
 ::Number of cores is divided by 2 for keeping the system responsive/preventing Intel Hyperthreading to confuse us
 set /A a = %NUMBER_OF_PROCESSORS%
 set /A b = a/2
 echo Building with %b% cores.
+
+for /f "tokens=* delims=" %%x in (rsrc/buildnumber.txt) do (
+ set %x%+=1
+)
+
+echo %x%
 
 ::x86 - Generic
 %BPP_COMPILER%/mingw64/bin/mingw32-make -f makefile.windows -j%b% -B FLAGS="-w -Wall -Wextra -pedantic -fpermissive -pg -g3 -Ofast -flto -m64 -mavx2 -static-libstdc++ -static-libgcc"
--- scripts/test.bat
+++ scripts/test.bat
@@ -1,3 +1,8 @@
-set /A a = %NUMBER_OF_PROCESSORS%
-set /A b = a/2
-echo Building with %b% cores.
+set /p content=<rsrc/buildnumber.txt
+set /A content=content+1
+(echo %content%)>rsrc/buildnumber2.txt
+cd rsrc
+del buildnumber.txt
+rename buildnumber2.txt buildnumber.txt
+
+cd %BPP_ROOT%
