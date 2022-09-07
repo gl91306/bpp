3cf1eb45879856405b09089eb56a53e74e55cdbc
Akshat Singh <HackerDaGreat57@gmail.com>
2022-09-06 20:14:37 -0700

    New build: v0.0.0.13

--- scripts/build.windows.bat
+++ scripts/build.windows.bat
@@ -15,24 +15,32 @@ cd bin
 if exist zlib1.dll (
   echo DLLs already exist inside bin directory
 ) else (
-  xcopy "%BPP_COMPILER%/mingw64/bin/*.dll" "%BPP_ROOT%/build" /f /g /y
+  xcopy "%BPP_COMPILER%/mingw64/bin/*.dll" "%BPP_ROOT%/bin/" /f /g /y /h
 )
+
+cd %BPP_ROOT%
 
 ::Number of cores is divided by 2 for keeping the system responsive/preventing Intel Hyperthreading to confuse us
 set /A b = %NUMBER_OF_PROCESSORS%/2
 echo Building with %b% cores.
 
-set /p content=<rsrc/buildnumber.txt
-set /A content=content+1
+::Increment & save the build number
+set /p old=<rsrc/buildnumber.txt
+set /A content=old+1
 (echo %content%)>rsrc/buildnumber2.txt
-
 cd rsrc
 del buildnumber.txt
 rename buildnumber2.txt buildnumber.txt
 
 cd /D %BPP_ROOT%
 
-::Ok, the build has been set up. Let's actually compile the stuff now.
+::Increment the build number in the source code file
+powershell -Command "(gc "%BPP_ROOT%/src/bpp.cpp") -replace 'v0.0.0.%old%', 'v0.0.0.%content%' | Out-File -encoding ASCII "%BPP_ROOT%/src/bpp.cpp""
 
-::x86 - Generic
+::Ok, the build has been set up. Let's actually compile the stuff now.
 %BPP_COMPILER%/mingw64/bin/mingw32-make -f makefile.windows -j%b% FLAGS="-w -Wall -Wextra -pedantic -fpermissive -pg -g3 -Ofast -flto -m64 -mavx2"
+
+::Commit to the repository
+git add .
+git commit -m "New build: v0.0.0.%content%"
+git push
