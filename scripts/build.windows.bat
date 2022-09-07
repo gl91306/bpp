1f1ebe689b5479b944fee07b472fcd1d5e102158
Akshat Singh <HackerDaGreat57@gmail.com>
2022-09-07 15:58:52 -0700

    New build: v0.0.0.55

--- scripts/build.windows.bat
+++ scripts/build.windows.bat
@@ -39,7 +39,7 @@ powershell -Command "(gc "%BPP_ROOT%/src/bpp.cpp") -replace 'v0.0.0.%old%', 'v0.
 powershell -Command "(gc "%BPP_ROOT%/rsrc/bpp.rc") -replace '%old%', '%content%' | Out-File -encoding ASCII "%BPP_ROOT%/rsrc/bpp.rc""
 
 ::Ok, the build has been set up. Let's actually compile the stuff now.
-%BPP_COMPILER%/mingw64/bin/mingw32-make -f makefile.windows -j%b%
+%BPP_COMPILER%/mingw64/bin/mingw32-make -f makefile.windows -j%b% X86_64
 
 ::Commit to the repository
 git add .
